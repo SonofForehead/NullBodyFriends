cfa81ad666c0b07c422732fb08859879609a9fde
Son of Forehead <jpstoner@icloud.com>
2024-10-25 22:55:51 -0600

    Add project files.

---
+++ NullBodyFriends/Core.cs
@@ -0,0 +1,53 @@
+﻿using BoneLib.BoneMenu;
+using MelonLoader;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(NullBodyFriends.Core), "NullBodyFriends", "1.0.0", "SonofForehead", null)]
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
+
+namespace NullBodyFriends
+{
+    public class Core : MelonMod
+    {
+        public static bool toggleMod;
+
+        public static bool avatarIsNullBody;
+
+        public override void OnInitializeMelon()
+        {
+            CreateBonemenu();
+        }
+
+        public static void CreateBonemenu()
+        {
+            Page mainPage = Page.Root.CreatePage("NullBody Friends", Color.yellow);
+            mainPage.CreateBool("Mod Toggle", Color.white, true, ModToggle);
+        }
+
+        public static void ModToggle(bool toggle)
+        {
+            toggleMod = toggle;
+
+            if (toggleMod)
+            {
+                MelonLogger.Msg("Mod Enabled");
+            }
+            else
+            {
+                MelonLogger.Msg("Mod Disabled");
+            }
+        }
+        public override void OnUpdate()
+        {
+            if (BoneLib.Player.RigManager && BoneLib.Player.RigManager.AvatarCrate.Crate.Title.ToLower().Contains("null"))
+            {
+                avatarIsNullBody = true;
+
+            }
+            else
+            {
+                avatarIsNullBody = false;
+            }
+        }
+    }
+}
---
+++ NullBodyFriends/Patches.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Harmony;
+using HarmonyLib;
+using BoneLib;
+using Il2CppSLZ.AI;
+using Il2CppSLZ.Marrow.AI;
+using Il2CppSLZ.VRMK;
+using Il2CppTriangleNet;
+using Il2CppPuppetMasta;
+using Il2CppSLZ.Marrow.PuppetMasta;
+using UnityEngine;
+using Il2CppSLZ.Marrow;
+using MelonLoader;
+
+namespace NullBodyFriends.Patches
+{
+    [HarmonyLib.HarmonyPatch(typeof(RigManager), "SwitchAvatar")]
+    public class TriggerRefPatch
+    {
+        public static void Postfix(TriggerRefProxy __instance)
+        {
+            MelonLogger.Msg("TriggerRefProxy Avatar Switched");
+
+            if (BoneLib.Player.RigManager.AvatarCrate.Crate.Title != "PolyBlank")
+            {
+                __instance.triggerType = TriggerRefProxy.TriggerType.Player;
+                if (NullBodyFriends.Core.avatarIsNullBody)
+                {
+                    MelonLogger.Msg("npcType swapped to FordHair");
+                    __instance.npcType = TriggerRefProxy.NpcType.FordHair;
+                }
+                else
+                {
+                    MelonLogger.Msg("npcType swapped to nullbody");
+                    __instance.npcType = TriggerRefProxy.NpcType.NullBody;
+                }
+            }
+        }
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(RigManager), "SwitchAvatar")]
+    public class PowerLegsPatch
+    {
+        public static void Postfix(BehaviourPowerLegs __instance)
+        {
+            MelonLogger.Msg("BehaviourPowerLegs Avatar Switched");
+
+            if (BoneLib.Player.RigManager.AvatarCrate.Crate.Title != "PolyBlank")
+            {
+                if (NullBodyFriends.Core.avatarIsNullBody)
+                {
+                    MelonLogger.Msg("agroOnNpcType Swaped");
+                    __instance.agroOnNpcType -= TriggerRefProxy.NpcType.FordHair;
+                    __instance.agroOnNpcType = TriggerRefProxy.NpcType.NullBody;
+                }
+                else
+                {
+                    MelonLogger.Msg("agroOnNpcType Swaped AGAIN");
+                    __instance.agroOnNpcType = TriggerRefProxy.NpcType.FordHair;
+                    __instance.agroOnNpcType -= TriggerRefProxy.NpcType.NullBody;
+                }
+            }
+        }
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(RigManager), "SwitchAvatar")]
+    public class SubBehaviourPatch
+    {
+        public static void Postfix(SubBehaviourHealth __instance)
+        {
+            MelonLogger.Msg(NullBodyFriends.Core.avatarIsNullBody);
+            MelonLogger.Msg(BoneLib.Player.RigManager.AvatarCrate.Crate.Title);
+            MelonLogger.Msg("AvatarSwitched");
+            if (BoneLib.Player.RigManager.AvatarCrate.Crate.Title != "PolyBlank")
+            {
+                if (NullBodyFriends.Core.avatarIsNullBody)
+                {
+                    MelonLogger.Msg("Aggression 0");
+                    __instance.aggression = 0;
+                }
+                else
+                {
+                    MelonLogger.Msg("Aggression 1");
+                    __instance.aggression = 1;
+                }
+            }
+        }
+    }
+}
